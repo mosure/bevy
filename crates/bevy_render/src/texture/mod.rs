90c91fb5772467cbbe4519189278a78ec3fadcb6
mosure <mitchell@mosure.me>
2024-05-24 00:56:40 -0500

    Revert "feat: change default texture format"
    This reverts commit 90f1646f579cf209d4274ad1fe5dbf8bf039cbf5.

--- crates/bevy_render/src/texture/mod.rs
+++ crates/bevy_render/src/texture/mod.rs
@@ -161,6 +161,6 @@ pub trait BevyDefault {
 
 impl BevyDefault for wgpu::TextureFormat {
     fn bevy_default() -> Self {
-        wgpu::TextureFormat::Bgra8Unorm
+        wgpu::TextureFormat::Rgba8UnormSrgb
     }
 }
